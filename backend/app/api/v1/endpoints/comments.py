76054a1804651e4e14a55f11cabf820d6e9729f7
artur <aartur.now@gmail.com>
2025-12-02 18:31:38 +0100

    fix: correct endpoints urls

--- backend/app/api/v1/endpoints/comments.py
+++ backend/app/api/v1/endpoints/comments.py
@@ -7,21 +7,21 @@ api_router = APIRouter(
     tags=['comments']
 )
 
-@api_router.get("/comments/", response_model=List[CommentDetails])
+@api_router.get("/", response_model=List[CommentDetails])
 def get_comments():
     raise HTTPException(501, "Not implemented")
 
 
-@api_router.get("/comments/{comment_id}/", response_model=CommentDetails)
+@api_router.get("/{comment_id}/", response_model=CommentDetails)
 def get_task(comment_id: int):
     raise HTTPException(501, "Not implemented")
 
 
-@api_router.post("/comments/")
+@api_router.post("/")
 def create_comment(comment: Annotated[CreateComment, Body()]):
     raise HTTPException(501, "Not implemented")
 
 
-@api_router.delete("/comments/{comment_id}/")
+@api_router.delete("/{comment_id}/")
 def create_comment(comment_id: int):
     raise HTTPException(501, "Not implemented")
--- backend/app/api/v1/endpoints/tasks.py
+++ backend/app/api/v1/endpoints/tasks.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Depends
+from db.session import get_db
 from schemas.task import CreateTask, TaskSimple, TaskDetails
 from typing import List
 
@@ -7,26 +8,26 @@ api_router = APIRouter(
     tags=['tasks']
 )
 
-@api_router.get("/tasks/", response_model=List[TaskSimple])
+@api_router.get("/", response_model=List[TaskSimple])
 def get_tasks():
     raise HTTPException(501, "Not implemented")
 
 
-@api_router.get("/tasks/{task_id}/", response_model=TaskDetails)
+@api_router.get("/{task_id}/", response_model=TaskDetails)
 def get_task(task_id: int):
     raise HTTPException(501, "Not implemented")
 
 
-@api_router.post("/tasks/")
+@api_router.post("/")
 def create_task(task: CreateTask):
     raise HTTPException(501, "Not implemented")
 
 
-@api_router.put("/tasks/{task_id}")
+@api_router.put("/{task_id}")
 def update_task(task_id: int):
     raise HTTPException(501, "Not implemented")
 
 
-@api_router.delete("/tasks/{task_id}")
+@api_router.delete("/{task_id}")
 def delete_task(task_id: int):
     raise HTTPException(501, "Not implemented")
