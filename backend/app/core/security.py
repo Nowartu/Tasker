1bb90fcfc58308c4552f75e092bce0cf568e3233
artur <aartur.now@gmail.com>
2025-12-03 19:54:22 +0100

    feat: Added OAuth2

---
+++ backend/app/core/security.py
@@ -0,0 +1,14 @@
+from fastapi.security import OAuth2PasswordBearer
+from pwdlib import PasswordHash
+
+password_hash = PasswordHash.recommended()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/v1/users/token/")
+
+
+def verify_password(plain_password, hashed_password):
+    return password_hash.verify(plain_password, hashed_password)
+
+
+def get_password_hash(password):
+    return password_hash.hash(password)
