8cfe1c1e1b79b8cd7e9e988e4353749d10185208
artur <aartur.now@gmail.com>
2025-12-07 10:17:53 +0100

    refactor: change models from SQLAlchemy 1.x to 2.x

--- backend/app/models/comment.py
+++ backend/app/models/comment.py
@@ -12,7 +12,7 @@ class Comment(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
     task_id: Mapped[int] = mapped_column(Integer, ForeignKey("tasks.id"))
-    task: Mapped[str] = relationship('Task', back_populates='comments')
+    task = relationship('Task', back_populates='comments')
 
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     created_by: Mapped[datetime] = mapped_column(String(100), nullable=False)
