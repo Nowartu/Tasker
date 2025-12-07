700280a81da2f881469fec54b548f0855c6b5344
artur <aartur.now@gmail.com>
2025-12-07 10:37:44 +0100

    refactor: change models from SQLAlchemy 1.x to 2.x

--- backend/app/models/task.py
+++ backend/app/models/task.py
@@ -12,13 +12,13 @@ class Task(Base):
     id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
 
     done: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    done_at: Mapped[datetime] = mapped_column(DateTime)
-    done_by: Mapped[str] = mapped_column(String(100))
+    done_at: Mapped[datetime | None] = mapped_column(DateTime)
+    done_by: Mapped[str | None] = mapped_column(String(100))
 
     created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
     created_by: Mapped[datetime] = mapped_column(String(100), nullable=False)
 
     title = mapped_column(String(250), nullable=False)
-    description: Mapped[str] = mapped_column(Text)
+    description: Mapped[str | None] = mapped_column(Text)
 
     comments = relationship('Comment', back_populates='task')
